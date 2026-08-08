124477fa9b6e6598f871d3f3edffa1e84f04e0f6
lavanya7682 <lavanyabhuria@gmail.com>
2026-08-08 22:22:36 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
+import win32com.client as wincom
+speak = wincom.Dispatch("SAPI.SpVoice")
+
+text = "Python text-to-speech test. using win32com.client"
+speak.Speak(text)
+
 
 API_KEY = "b13989793f184149a91141538230103"
-CITY = "mohali"
+CITY = "dharmshala himachal"
 
 # Correct URL for WeatherAPI.com
 URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}"
@@ -12,6 +18,8 @@ if response.status_code == 200:
     data = response.json()
     temp_c = data["current"]["temp_c"]
     condition = data["current"]["condition"]["text"]
-    print(f"Weather in {CITY}: {condition}, {temp_c}°C")
+    weather_text=f"Weather in {CITY}: {condition}, {temp_c}°C"
+    print(weather_text)
+    speak.Speak(weather_text)
 else:
     print("Error:", response.json())
